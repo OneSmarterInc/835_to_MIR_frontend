de9259fd55a9191df9428622d1c90785cdda0a4b
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-23 16:14:11 +0530

    Apply demo PHI encoding to universal claim search

--- src/onesmarter_admin/services/claimSearchApi.js
+++ src/onesmarter_admin/services/claimSearchApi.js
@@ -1,3 +1,5 @@
+import { encodeDemoData } from '../../utils/demoEncoder';
+
 function getAuthHeaders() {
   const token = localStorage.getItem('onesmarter_admin_token');
   const headers = {};
@@ -22,5 +24,5 @@ export async function searchUniversalClaims(clientId, query = '', field = 'all',
   });
   const data = await res.json().catch(() => ({}));
   if (!res.ok || !data.success) throw new Error(data.error || 'Unable to load universal claims.');
-  return data;
+  return encodeDemoData(data);
 }
